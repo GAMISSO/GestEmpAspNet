6de305d28ca30c1895fd85a4a05c7e8d1f755b57
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2025-12-02 15:22:42 +0100

    ReposiOne

--- GestEmpAspNet/Controllers/DepartementController.cs
+++ GestEmpAspNet/Controllers/DepartementController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Models;
 using Services;
 
 namespace GestEmpAspNet.Controllers
@@ -6,16 +7,28 @@ namespace GestEmpAspNet.Controllers
     public class DepartementController :Controller
     {
         private readonly IDepartementService _departementService;
+        private readonly ILogger<DepartementController> _logger;
 
-        public DepartementController(IDepartementService departementService)
+        public DepartementController(IDepartementService departementService,ILogger<DepartementController> logger)
         {
             _departementService = departementService;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                var departements = _departementService.GetDepartements();
+                return View(departements);
+            }
+            catch (Exception)
+            {
+                // Log the exception (not implemented here for brevity)
+                _logger.LogError("An error occurred while loading the departement index.");
+                return View(new List<Departement>());
+            };
         }
         [HttpPost]
         public IActionResult Create()
